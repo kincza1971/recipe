9651836cf2d87e44f2762d5d3a6359242a5f8ab3
kincza <kincza@gmail.com>
2021-08-03 15:24:25 +0200

    Vizsgaremek, new entity ingredients

---
+++ src/main/java/recipe/Ingredient.java
@@ -0,0 +1,45 @@
+package recipe;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ingredients")
+public class Ingredient {
+
+    public enum MeasurementUnit {GRAM, KG, TABLESPOON, TEASPOON, COFFEE_SPOON, CUPFUL, PIECE, PIECES}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long recipe_id;
+
+    @Column(name = "ing_name")
+    private String name;
+
+    @Column(name = "qty")
+    private double quantity;
+
+    @Column(name = "unit")
+    @Enumerated(value = EnumType.STRING)
+    private MeasurementUnit unit;
+}
+
+//CREATE TABLE `ingredients` (
+//	`id` BIGINT NOT NULL AUTO_INCREMENT,
+//	`recipe_id` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+//	`ing_name` VARCHAR(255) NOT NULL,
+//	`qty` DOUBLE NOT NULL,
+//	`unit` VARCHAR(50) NOT NULL,
+//	PRIMARY KEY (`id`),
+//	CONSTRAINT `fk_recipe` FOREIGN KEY (`recipe_id`) REFERENCES `recipes` (`id`) ON UPDATE NO ACTION ON DELETE NO ACTION
+//)
+//COLLATE='utf8mb3_hungarian_ci'
+//;
